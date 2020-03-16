a1d712a12f8e804509aae30a9aebd6f826d5cd35
Matt Eden <mrmatteden@gmail.com>
2020-03-17 08:23:23 +1300

    Create repo and add all files from local

---
+++ src/App.jsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import './Frosted.scss';
+
+const bgImage = "https://source.unsplash.com/collection/1065396/1600x900";
+const bgImageValue = `url(${bgImage})`;
+
+class FrostedGlassView extends React.Component {
+    render() {
+        return (
+            <div style={!this.props.disabled ? { backgroundColor: bgImageValue, backgroundImage: bgImageValue } : {}}
+                className={"bg frosted-glass " + (this.props.className ? this.props.className : "")}
+            >
+                <div className="frosted-glass-content">
+                    {this.props.children}
+                </div>
+            </div>
+        );
+    }
+}
+
+class App extends React.Component {
+    constructor() {
+        super();
+        
+        this.state = {
+            disabled: false
+        };
+    }
+    
+    render() {
+        return (
+            <main className="bg has-frosted-glass" style={{backgroundImage: bgImageValue}}>
+                <FrostedGlassView disabled={this.state.disabled} className="my-glass">
+                    <h1>Frosted Glass as React Component</h1>
+                    
+                    <p>
+                        I'm a big fan of the Frosted Glass effect. With this Component
+                        you can easily use it in your React projects. To get it just
+                        grab the &lt;FrostedGlassView /&gt; component and the first part
+                        of the stylesheet.<br />
+                        Unfortunately there are still some issues of using it in some browsers.
+                        For example if you have scrollable content Edge and Firefox will get 
+                        stucked with the background image. That's why you can disable the 
+                        effect with the <code>disabled</code> property.
+                    </p>
+                    
+                    <label>
+                        <input 
+                            type="checkbox" 
+                            checked={this.state.disabled} 
+                            onChange={() => this.setState({disabled: !this.state.disabled})}
+                        />
+                        &nbsp;Disable frosted glass effect
+                    </label>
+                </FrostedGlassView>
+            </main>
+        );
+    }
+}
+
+export default App;
