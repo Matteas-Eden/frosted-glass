78fd9ad888adbdbf5abea804dbd61618f0a2e1ef
Matt Eden <mrmatteden@gmail.com>
2020-03-17 08:38:49 +1300

    Updates

---
+++ precache-manifest.65cb78c88aa1a4b1bbd5c20bd3a5f6c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c4eddee24282eccd5a2ca3cc8cf7592",
+    "url": "/index.html"
+  },
+  {
+    "revision": "111d9f087cefd7991d01",
+    "url": "/static/css/main.798102be.chunk.css"
+  },
+  {
+    "revision": "7fe0845a347327439809",
+    "url": "/static/js/2.80027728.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.80027728.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "111d9f087cefd7991d01",
+    "url": "/static/js/main.dd097e60.chunk.js"
+  },
+  {
+    "revision": "19133a5d6c7be98533aa",
+    "url": "/static/js/runtime-main.107bb8ca.js"
+  }
+]);
