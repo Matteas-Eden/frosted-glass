db2ac8a1402a52cf496f183d20762508924d5bdd
Matt Eden <mrmatteden@gmail.com>
2020-03-23 17:21:10 +1300

    Move component outside of App

---
+++ src/FrostedGlassView.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export const FrostedGlassView = (props) => {
+  return (
+    <div
+      style={
+        !props.disabled
+          ? { backgroundColor: props.bg, backgroundImage: props.bg }
+          : {}
+      }
+      className={"bg frosted-glass " + (props.className ? props.className : "")}
+    >
+      <div className="frosted-glass-content">{props.children}</div>
+    </div>
+  );
+};
